560db4fd3a80cd81a9f4898301fe88ec83b10ce8
maxjschuh <maximilianschuh@gmx.de>
2023-08-11 16:24:25 +0200

    added classes and basic canvas drawing

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let canvas;
+let world;
+
+function init() {
+
+    canvas = document.getElementById('canvas');
+    world = new World(canvas);
+
+
+    // setTimeout(() => {
+    //     ctx.drawImage(character, 20, 20, 50, 150); 
+
+    // }, 2000);
+
+}
+
+
+
+
+
+
+
---
+++ models/chicken.class.js
@@ -0,0 +1,10 @@
+class Chicken extends MovableObject {
+
+    constructor() {
+        super().loadImage('../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+    }
+    
+    eat() {
+
+    }
+}
