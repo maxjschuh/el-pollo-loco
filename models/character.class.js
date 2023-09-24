65ad8fb12b31402d8f8c0e9b74d0234469b910de
maxjschuh <maximilianschuh@gmx.de>
2023-09-24 22:27:42 +0200

    changed graphics design

--- models/character.class.js
+++ models/character.class.js
@@ -4,6 +4,14 @@ class Character extends MovableObject {
 
     IMAGES_IDLE = [
         '../img/2_character_pepe/1_idle/idle/I-1.png',
+        '../img/2_character_pepe/1_idle/idle/I-2.png',
+        '../img/2_character_pepe/1_idle/idle/I-3.png',
+        '../img/2_character_pepe/1_idle/idle/I-4.png',
+        '../img/2_character_pepe/1_idle/idle/I-5.png',
+        '../img/2_character_pepe/1_idle/idle/I-6.png',
+        '../img/2_character_pepe/1_idle/idle/I-7.png',
+        '../img/2_character_pepe/1_idle/idle/I-8.png',
+        '../img/2_character_pepe/1_idle/idle/I-10.png'
     ];
 
     IMAGES_SWIM = [
@@ -15,21 +23,18 @@ class Character extends MovableObject {
         '../img/1.Sharkie/3.Swim/6.png'
     ];
     IMAGES_SLEEP = [
-        '../img/1.Sharkie/2.Long_IDLE/i1.png',
-        '../img/1.Sharkie/2.Long_IDLE/i2.png',
-        '../img/1.Sharkie/2.Long_IDLE/i3.png',
-        '../img/1.Sharkie/2.Long_IDLE/i4.png',
-        '../img/1.Sharkie/2.Long_IDLE/i5.png',
-        '../img/1.Sharkie/2.Long_IDLE/i6.png',
-        '../img/1.Sharkie/2.Long_IDLE/i7.png',
-        '../img/1.Sharkie/2.Long_IDLE/i8.png',
-        '../img/1.Sharkie/2.Long_IDLE/i9.png',
-        '../img/1.Sharkie/2.Long_IDLE/i10.png',
-        '../img/1.Sharkie/2.Long_IDLE/i11.png',
-        '../img/1.Sharkie/2.Long_IDLE/i12.png',
-        '../img/1.Sharkie/2.Long_IDLE/i13.png',
-        '../img/1.Sharkie/2.Long_IDLE/i14.png'
+        '../img/2_character_pepe/1_idle/long_idle/I-11.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-12.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-13.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-14.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-15.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-16.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-17.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-18.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-19.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-20.png'
     ];
+    
     IMAGES_HURT_POISONED = [
         '../img/1.Sharkie/5.Hurt/1.Poisoned/2.png',
         '../img/1.Sharkie/5.Hurt/1.Poisoned/3.png',
