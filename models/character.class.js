00fd487fb6c74d593a643b0d76aeffff327e04d0
maxjschuh <maximilianschuh@gmx.de>
2023-09-24 22:24:09 +0200

    changed graphics design

--- models/character.class.js
+++ models/character.class.js
@@ -3,24 +3,7 @@ class Character extends MovableObject {
     y = 80;
 
     IMAGES_IDLE = [
-        '../img/1.Sharkie/1.IDLE/1.png',
-        '../img/1.Sharkie/1.IDLE/2.png',
-        '../img/1.Sharkie/1.IDLE/3.png',
-        '../img/1.Sharkie/1.IDLE/4.png',
-        '../img/1.Sharkie/1.IDLE/5.png',
-        '../img/1.Sharkie/1.IDLE/6.png',
-        '../img/1.Sharkie/1.IDLE/7.png',
-        '../img/1.Sharkie/1.IDLE/8.png',
-        '../img/1.Sharkie/1.IDLE/9.png',
-        '../img/1.Sharkie/1.IDLE/10.png',
-        '../img/1.Sharkie/1.IDLE/11.png',
-        '../img/1.Sharkie/1.IDLE/12.png',
-        '../img/1.Sharkie/1.IDLE/13.png',
-        '../img/1.Sharkie/1.IDLE/14.png',
-        '../img/1.Sharkie/1.IDLE/15.png',
-        '../img/1.Sharkie/1.IDLE/16.png',
-        '../img/1.Sharkie/1.IDLE/17.png',
-        '../img/1.Sharkie/1.IDLE/18.png'
+        '../img/2_character_pepe/1_idle/idle/I-1.png',
     ];
 
     IMAGES_SWIM = [
